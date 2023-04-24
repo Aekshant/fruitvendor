bf4b17951b015ea0264485dc7c4812f93a6d440b
kalyan_dahake75 <kalyan.dahake@amantyatech.com>
2023-04-24 14:26:54 +0530

    radio buttons

--- frontend/src/componets/tools/RadioButton.js
+++ frontend/src/componets/tools/RadioButton.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./RadioButton.css"
 
 export const RadioButton = ( props ) => {
   return (
     <div className='RadioButton' { ...props }>
-        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" style={{ marginRight : ".35rem" }}/>
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" style={{ marginRight : ".35rem", backgroundColor: '#D14D72',  border: "10px solid #4C6793", }}/>
         <label class="form-check-label" for="flexRadioDefault1"> { props.name }</label>
     </div>
   )
