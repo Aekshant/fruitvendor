caf8d52a0a7a00209ac2a00f7126ea6063479111
Aekshant <dahakekalyan75@gmail.com>
2023-04-25 00:28:35 +0530

    done with checked issue

--- frontend/src/componets/assets/GetApp.js
+++ frontend/src/componets/assets/GetApp.js
@@ -6,14 +6,9 @@ import { InputBox } from "../tools/InputBox"
 import { DownloadApp } from "./DownloadApp"
 
 export const GetApp = () => {
-  const [ checkedList, setChecked ] = useState( { email : false, whatsApp: false } )
+  const [ checkedWay, setChecked ] = useState( "email" )
   const handleCheck = ( data ) => {
-    console.log(data);
-    // const stateData = checkedList
-    // stateData[ data ] = ( data == "email" ? true : false )
-    // stateData[ data ] = ( data == "whatsApp" ? true : false )
-    // // console.log(stateData);
-    // setChecked( stateData )
+    if( typeof data === "string" ) setChecked( data )
   }
   return (
     <InlineContainer style={{ justifyContent: "center" }}>
@@ -22,10 +17,10 @@ export const GetApp = () => {
             <h3 style={{ marginBottom: "5%"  }}>Get the Vendor's app</h3>
             <p style={{ fontSize: "1.2rem", marginBottom: "5%"  }}>We will send you a link, open it on your phone to download the app</p>
             <InlineContainer style={{ marginBottom: "5%" }}>
-                <RadioButton name="Email" value="email" style={ { marginRight: "6%" } } checked={ checkedList.email } onChange={ handleCheck } />
-                <RadioButton name="WhatsApp" value="whatsApp" checked={ checkedList.whatsApp } onChange={ handleCheck }/>
+                <RadioButton name="Email" value="email" style={ { marginRight: "6%" } } checked={ checkedWay == "email" } onChange={ handleCheck } />
+                <RadioButton name="WhatsApp" value="whatsApp" checked={ checkedWay == "whatsApp" } onChange={ handleCheck }/>
             </InlineContainer>
-            <InputBox name="Share App Link" style={{ marginBottom: "5%" }}/>
+            <InputBox name="Share App Link" style={{ marginBottom: "5%" }} placeholder = { checkedWay == "email" ? "Enter Email Address" : "Enter What's App Number" } />
             <h5 style={ { color : 'grey', marginBottom: "5%" } }>Download App from</h5>
             <div style={{ display: "flex", justifyContent: "space-between", marginRight : "10%" }}>
               <DownloadApp/>
--- frontend/src/componets/tools/RadioButton.js
+++ frontend/src/componets/tools/RadioButton.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import "./RadioButton.css"
 
-export const RadioButton = ( props ) => {
- const data = ( ) => { 
-  props.onChange( props.value ) 
-}
-    
+export const RadioButton = ( props ) => {    
+  const onChecked =( e ) => {
+    props.onChange( e.target.name )
+  }
   return (
-    <div className='RadioButton' { ...props }>
-        <input className="form-check-input" type="radio" name={ props.value } style={{ marginRight : ".35rem" }} checked={ props.checked } onChange={ data }/>
+    <div className='RadioButton' { ...props } key={props.value}>
+        <input className="form-check-input" type="radio" name={ props.value } style={{ marginRight : ".35rem" }} checked={ props.checked } onChange={ onChecked }/>
         <label className="form-check-label" htmlFor="flexRadioDefault1"> { props.name }</label>
     </div>
   )
